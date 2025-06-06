276f9810c9c2d69ad04b195df8d6e705e3354970
FrostLegend <alejandro11valla@gmail.com>
2025-06-06 22:56:53 +0200

    operaciones

--- script.js
+++ script.js
@@ -7,6 +7,31 @@ for (let boton of botones) {
         if (botonID != 'C' && botonID != '=') {
             pantalla.value += botonID;
 
+            if (isNaN(pantalla.value.charAt(0)) && pantalla.value.charAt(0) != '-') { //Esto se hace por si quieres poner como primer valor un operador o el punto
+                pantalla.value = '';
+            }
+
+            switch (pantalla.value.slice(-2, -1)) { //Esto se hace cuando el penultimo valor es un operador o punto
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '.': //En javaScript no se puede poner un case con mas de una condicion
+                    switch (botonID) { //Y este se hace para decirme el ultimo valor
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                        case '.': //Si cuadra los dos casos me sustituye los 2 utlimos valores por el ultimo
+                            pantalla.value = pantalla.value.slice(0, -2) + botonID;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
         } else if (botonID == '=') {
             if (pantalla.value == '') {
                 pantalla.value = 0;
